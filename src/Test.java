9aa2a69e198de9c9d7c56d17a439ca08a02fd4c7
heekzz <f.hakansson@hotmail.se>
2014-11-22 14:23:11 +0100

    First

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		String x = "Hej";
+		String y = "tja"; 
+		System.out.println(x.hashCode());
+		System.out.println(y.hashCode());
+	}
+
+}
